3c1d6269366d0a76a245d1f90925981c0be9b347
unknown <judenkereuwem@gmail.com>
2022-10-03 11:44:42 +0100

    potentiometer code

--- Code/pico_potentiometer.py
+++ Code/pico_potentiometer.py
@@ -1,13 +1,9 @@
-from machine import ADC, Pin, PWM
+
+from machine import ADC, Pin
 import time
 
-pwm = PWM(Pin(15))
 adc = ADC(Pin(26))
 
-pwm.freq(1000)
-
 while True:
-    #print(adc.read_u16())
-    #time.sleep(1)
-    duty = adc.read_u16()
-    pwm.duty_u16(duty)
+    print(adc.read_u16())
+    time.sleep(1)
